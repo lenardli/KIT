ce42de801794eff376f620f1abc9a94c3812ac00
Арина Шейнова <luzganova81@mail.ru>
2024-02-03 23:55:25 +0300

    first commit

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from database.common.models import db, History
 from database.core import crud
 from tg_API.core import Bot
 import requests
-
+from test import hh
 
 # db_write = crud.create()
 # db_read = crud.retrieve_all()
-
-Bot()
+print(hh)
+# Bot()
 # headers = {'User-Agent': 'api-test-agent'}
 # params = {"clusters": "true"}
 # resp = requests.get(url='https://api.hh.ru/vacancies', headers=headers, params=params)
